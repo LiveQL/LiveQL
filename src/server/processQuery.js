81cb8932053f99df80524129a54c7b1c290b4048
Andrew <afuselier23@gmail.com>
2018-03-13 20:30:35 -0700

    incoming middleware function

--- src/server/processQuery.js
+++ src/server/processQuery.js
@@ -6,19 +6,18 @@ const queryHash = require('./queryHash');
  * identifier. It stores that "hash" with the query string in an object that is used
  * to subscribe to data and reply when data is changed.
  *
- * @param {req} req - Express req object. req.body.query stores the GraphQL query string.
- * @param {*} res - Express res object.
- * @param {*} next - Express next function
+ * @param {Object} req - Express req object. req.body.query stores the GraphQL query string.
+ * @param {Object} res - Express res object.
+ * @param {Function} next - Express next function.
  */
 module.exports = (req, res, next) => {
   let { query } = req.body;
   if (!query) return next();
   query = query.trim();
-
   const { variables } = req.body;
   const open = query.indexOf('{');
 
-  // This is a not a query, so it cannot subscribe to data
+  // This is a not a query, so it cannot subscribe to data.
   if (open !== 0 && !query.slice(0, open).includes('query')) return next();
 
   /**
@@ -27,10 +26,16 @@ module.exports = (req, res, next) => {
    * of config and look for it here.
    */
   if (!query.slice(0, open).includes('@live')) return next();
-  // Get hash of query
+
+  // Get hash of query.
   const hash = queryHash(query, variables);
 
-  queue[hash] = { query, variables };
+  // There's at least one subscriber on this query.
+  if (queue[hash]) {
+    queue[hash].listeners += 1;
+  } else {
+    queue[hash] = { query, variables, listeners: 1 };
+  }
   res.locals.handle = hash;
   return next();
 };
--- test/processQuery.test.js
+++ test/processQuery.test.js
@@ -1,9 +1,8 @@
 const processQuery = require('./../src/server/processQuery');
-const { queue } = require('./reactiveDataLayer');
-const queryHash = require('./queryHash');
+const queryHash = require('./../src/server/queryHash');
+const { queue } = require('./../src/server/reactiveDataLayer');
 
-// Setup fake req, res, and next variables.
-
+// Setup fake req, res, queue, and next variables.
 const req = {};
 const res = {};
 const next = () => undefined;
@@ -12,3 +11,56 @@ req.body = {};
 res.locals = {};
 
 
+test('Simple query, one user', () => {
+  req.body.query = `
+    query @live {
+      Topics {
+        content
+        id
+      }
+    }
+  `;
+  processQuery(req, res, next);
+  const hash = queryHash(req.body.query);
+  expect(queue[hash]).toBeTruthy();
+  expect(queue[hash].listeners).toEqual(1);
+  expect(queue[hash].query).toEqual(req.body.query.trim());
+});
+
+test('Simple query, two users', () => {
+  req.body.query = `
+    query @live {
+      Topics {
+        content
+        id
+      }
+    }
+  `;
+  processQuery(req, res, next);
+  const hash = queryHash(req.body.query);
+  expect(queue[hash]).toBeTruthy();
+  expect(queue[hash].listeners).toEqual(2);
+  expect(queue[hash].query).toEqual(req.body.query.trim());
+});
+
+
+test('Nested query with variables', () => {
+  req.body.query = `
+    query Testing($id: ID!) @live {
+      Topic(id: $id) {
+        id
+        author
+        comments {
+          id
+          author
+        }
+      }
+    }
+  `;
+  req.body.variables = { id: '5a9b26de4d33148fb6718928' };
+  processQuery(req, res, next);
+  const hash = queryHash(req.body.query, req.body.variables);
+  expect(queue[hash]).toBeTruthy();
+  expect(queue[hash].listeners).toEqual(1);
+  expect(queue[hash].query).toEqual(req.body.query.trim());
+});
